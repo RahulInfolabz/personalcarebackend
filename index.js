893f7139a7e3441b63722567b9128e83cb682345
rahulinfolabz <rahulinfolabz@gmail.com>
2025-04-02 23:58:15 +0530

    MODIFIED personal care

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "http://localhost:5174"], // Allowed frontend URLs
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:5173",
+      "http://localhost:5174",
+    ], // Allowed frontend URLs
     credentials: true, // Allow cookies and sessions to be shared across origins
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   })
