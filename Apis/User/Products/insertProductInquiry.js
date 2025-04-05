32bc1577dd95df3f38e2456c0a15f4841b7a1414
rahulinfolabz <rahulinfolabz@gmail.com>
2025-04-05 12:51:30 +0530

    PRODUCT INQUIRY IN PERSONAL CARE

--- Apis/User/Products/insertProductInquiry.js
+++ Apis/User/Products/insertProductInquiry.js
@@ -11,7 +11,7 @@ async function AddProductInquiry(req, res) {
     if (
       !productId ||
       !username ||
-      !email ||
+      !email || 
       !phone ||
       !message ||
       !qty ||
